c9fe6fdd86d20059c859028157edf4dbbf3df5ca
livia108 <105514024+livia108@users.noreply.github.com>
2023-10-11 14:47:28 -0300

    Add files via upload

---
+++ quiz/Projeto 1/questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Global Warm é:",
+      answers: [
+        { option: "Um problema para as autoridades resolverem.", correct: false },
+        { option: "Um problema para toda a sociedade resolver.", correct: true },
+        { option: "Um problema inventado, ou seja, uma teoria conspiração.", correct: false },
+        { option: "Um projeto do governo.", correct: false },
+      ],
+    },
+    {
+      question: "Quais das palavras abaixo você já conhece e que diz respeito às questões ambientais?",
+      answers: [
+        { option: "environment", correct: false },
+        { option: "recycle", correct: false },
+        { option: "pollution", correct: false },
+        { option: "todas as alternativas", correct: true },
+      ],
+    },
+    {
+      question: "Existem muitas palavras em inglês que se parecem com o português, porém é preciso ter cuidado, pois nem tudo que parece é. Elas são chamadas de “Falsos cognatos”. Marque a alternativa que contém uma dessas palavras.",
+      answers: [
+        { option: "Recycle", correct: false },
+        { option: "Realize", correct: true },
+        { option: "Crisis", correct: false },
+        { option: "Pollution", correct: false },
+      ],
+    },
+    {
+      question: "Complete com a palavra correta: “_____ the earth is in danger!”",
+      answers: [
+        { option: "actually", correct: true },
+        { option: "injury", correct: false },
+        { option: "ordinary", correct: false },
+        { option: "realize", correct: false },
+      ],
+    },
+    {
+      question: "Qual das palavras abaixo é uma cognata?",
+      answers: [
+        { option: "Realize", correct: false },
+        { option: "Pretend", correct: false },
+        { option: "Crisis", correct: true },
+        { option: "Push", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o sinônimo para Climate Crisis?",
+      answers: [
+        { option: "Crise climática", correct: true },
+        { option: "Aquecimento do clima", correct: false },
+        { option: "Aquecimento global", correct: false },
+        { option: "Crise no governo", correct: false },
+      ],
+    },
+    {
+      question: "Marque a alternativa correta.",
+      answers: [
+        { option: "Expert: sabido", correct: false },
+        { option: "Expert: esperto", correct: false },
+        { option: "Expert: especialista", correct: true },
+        { option: "Expert: externo", correct: false },
+      ],
+    },
+    {
+      question: "“People realize that pollution is destroying our planet.” Qual destas palavras é uma falsa incógnita?",
+      answers: [
+        { option: "pollution", correct: false },
+        { option: "realize", correct: true },
+        { option: "destroying", correct: false },
+        { option: "planet", correct: false },
+      ],
+    },
+    {
+      question: "Qual das alternativas abaixo contém o correto significado da palavra “Eventually”?",
+      answers: [
+        { option: "ocasionalmente", correct: false },
+        { option: "eventualmente", correct: true },
+        { option: "finalmente", correct: false },
+        { option: "socialmente", correct: false },
+      ],
+    },
+    {
+      question: "Qual das palavras a seguir é um falso cognato? support / arrange / international",
+      answers: [
+        { option: "support", correct: false },
+        { option: "arrange", correct: true },
+        { option: "international", correct: false },
+        { option: "todas as palavras", correct: false },
+      ],
+    },
+  ];
